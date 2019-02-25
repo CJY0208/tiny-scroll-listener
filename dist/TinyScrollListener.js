6cd7a93b0c0299c4c1a339974a8c0bd51e853865
CJY <375564567@qq.com>
2019-02-25 17:20:16 +0800

    - 重构 onEndReached 的实现，使用 distanceEvents 逻辑、优化代码压缩、增加注释

--- dist/TinyScrollListener.js
+++ dist/TinyScrollListener.js
@@ -6,118 +6,151 @@
 
   function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+  var isFunction = function isFunction(value) {
+    return typeof value === 'function';
+  };
+  var OUTSIDE = 'OUTSIDE';
+  var INSIDE = 'INSIDE';
+  var SCROLL_EVENT_NAME = 'scroll';
+
   var TinyScrollListener = function TinyScrollListener(_ref) {
+    var _this = this;
+
     var _ref$distanceToReachE = _ref.distanceToReachEnd,
         distanceToReachEnd = _ref$distanceToReachE === undefined ? 100 : _ref$distanceToReachE,
-        _ref$onEndReached = _ref.onEndReached,
-        onEndReached = _ref$onEndReached === undefined ? function () {
-      return undefined;
-    } : _ref$onEndReached,
+        onEndReached = _ref.onEndReached,
         element = _ref.element,
         _ref$distanceEvents = _ref.distanceEvents,
         distanceEvents = _ref$distanceEvents === undefined ? [] : _ref$distanceEvents;
 
     _classCallCheck(this, TinyScrollListener);
 
-    this.endReacher = {
-      working: true,
-      pausing: false,
-      pause: function pause() {
-        this.pausing = true;
-      },
-      continue: function _continue() {
-        this.pausing = false;
-      }
-    };
+    // 若无滚动载体，报错并退出
+    if (typeof element === 'undefined') {
+      console.error('Need Scroll Container!');
+      return;
+    }
 
-    var self = this;
-
-    if (typeof element === 'undefined') throw Error('Need Scroll-Container!');
-
+    // 判断滚动载体是否为 body
     var isBody = element === document.body;
-    var rAFLock = false;
-    var scrollHandler = void 0;
+
+    // 初始化滚动事件节点状态
     var distanceEventsStatus = distanceEvents.map(function (_ref2) {
       var distance = _ref2.distance;
-      return element.scrollTop > distance ? 'outside' : 'inside';
+      return element.scrollTop > distance ? OUTSIDE : INSIDE;
     });
 
-    self.destroy = function () {
-      if (isBody) {
-        document.removeEventListener('scroll', scrollHandler);
-      } else {
-        element.removeEventListener('scroll', scrollHandler);
-      }
-    };
+    /**
+     * 若使用触底函数，则启用相关逻辑
+     */
+    if (isFunction(onEndReached)) {
+      // 触底函数是否被冻结，将此值置为 true 则停止使用触底函数
+      var isEndReacherFreeze = false;
 
-    function onScroll(e) {
-      var _this = this;
+      /**
+       * 每次触发 onEndReached 函数后会自动阻止下一次触发，需要执行 done 函数来释放阻止
+       * （例如滚动到底部后开始做网络请求时，再次滚动到底部不会触发二次请求）
+       * 如果 isOver 为 true 则不会再触发后续的 onEndReached
+       * （例如已经加载了全部页，不需要再监听触底事件）
+       */
+      var done = function done(isOver) {
+        if (!isOver) {
+          isEndReacherFreeze = false;
+          return;
+        }
 
-      var scrollTop = isBody ? document.documentElement.scrollTop : this.scrollTop;
+        if (distanceEvents.length === 1) {
+          _this.destroy();
+        }
+      };
 
+      var endReachedEvents = {
+        distance: function distance() {
+          return element.scrollHeight - element.offsetHeight - distanceToReachEnd;
+        },
+
+        onGoingOut: function onGoingOut() {
+          if (isEndReacherFreeze) return;
+          isEndReacherFreeze = true;
+          onEndReached(done);
+        }
+      };
+
+      distanceEvents.push(endReachedEvents);
+      distanceEventsStatus.push(INSIDE);
+    }
+
+    // 若无滚动事件可用，报错并退出
+    if (distanceEvents.length === 0) {
+      console.error('Need Distance Events!');
+      return;
+    }
+
+    var onScroll = function onScroll(e) {
       e.stopPropagation();
 
+      // body 元素的 scrollTop 取值时不同于普通元素
+      var scrollTop = isBody ? document.documentElement.scrollTop : element.scrollTop;
+
+      // 每次 onScroll 触发时对各事件的监听
       distanceEvents.forEach(function (_ref3, idx) {
         var _ref3$distance = _ref3.distance,
             distance = _ref3$distance === undefined ? -1 : _ref3$distance,
             _ref3$onGoingIn = _ref3.onGoingIn,
             onGoingIn = _ref3$onGoingIn === undefined ? function () {
-          return console.log('on going in');
+          return undefined;
         } : _ref3$onGoingIn,
             _ref3$onGoingOut = _ref3.onGoingOut,
             onGoingOut = _ref3$onGoingOut === undefined ? function () {
-          return console.log('on going out');
+          return undefined;
         } : _ref3$onGoingOut;
 
+        if (isFunction(distance)) {
+          distance = distance();
+        }
+
+        // 仅当状态值变更时触发 onGoingIn、onGoingOut 函数
         switch (distanceEventsStatus[idx]) {
-          case 'inside':
+          case INSIDE:
             if (scrollTop > distance) {
-              onGoingOut.call(_this, self);
-              distanceEventsStatus[idx] = 'outside';
+              onGoingOut();
+              distanceEventsStatus[idx] = OUTSIDE;
             }
             break;
-          case 'outside':
+          case OUTSIDE:
             if (scrollTop <= distance) {
-              onGoingIn.call(_this, self);
-              distanceEventsStatus[idx] = 'inside';
+              onGoingIn();
+              distanceEventsStatus[idx] = INSIDE;
             }
             break;
         }
       });
+    };
 
-      if (self.endReacher.working && !self.endReacher.pausing && this.scrollTop + this.offsetHeight + distanceToReachEnd > this.scrollHeight) {
-        self.endReacher.pause();
-        onEndReached.call(this, function done(isOver) {
-          self.endReacher.continue();
-
-          if (isOver) {
-            if (distanceEvents.length === 0) {
-              self.destroy();
-            } else {
-              self.endReacher.working = false;
-            }
-          }
-        });
-      }
-    }
-
-    scrollHandler = function scrollHandler(e) {
+    /**
+     * 使用 requestAnimationFrame 优化 scroll 监听
+     */
+    var rAFLock = false;
+    var scrollHandler = function scrollHandler(e) {
       if (rAFLock) return;
       requestAnimationFrame(function () {
         onScroll.call(element, e);
         rAFLock = false;
       });
       rAFLock = true;
-    };
-
-    // .addEventListener('scroll', 在 body 上无效
-    if (isBody) {
-      document.addEventListener('scroll', scrollHandler);
-    } else {
-      element.addEventListener('scroll', scrollHandler);
     }
 
-    return self;
+    // .addEventListener('scroll', 在 body 上无效，需调用到 document 上
+    ;(isBody ? document : element).addEventListener(SCROLL_EVENT_NAME, scrollHandler);
+
+    this.destroy = function () {
+      return (
+        // .removeEventListener('scroll', 在 body 上无效，需调用到 document 上
+        (isBody ? document : element).removeEventListener(SCROLL_EVENT_NAME, scrollHandler)
+      );
+    };
+
+    return this;
   };
 
   return TinyScrollListener;
--- src/index.js
+++ src/index.js
@@ -1,84 +1,122 @@
+const isFunction = value => typeof value === 'function'
+const OUTSIDE = 'OUTSIDE'
+const INSIDE = 'INSIDE'
+const SCROLL_EVENT_NAME = 'scroll'
+
 export default class TinyScrollListener {
   constructor({
     distanceToReachEnd = 100,
-    onEndReached = () => undefined,
+    onEndReached,
     element,
     distanceEvents = []
   }) {
-    const self = this
-
-    if (typeof element === 'undefined') throw Error('Need Scroll-Container!')
-
-    const isBody = element === document.body
-    let rAFLock = false
-    let scrollHandler
-    let distanceEventsStatus = distanceEvents.map(
-      ({ distance }) => (element.scrollTop > distance ? 'outside' : 'inside')
-    )
-
-    self.destroy = () => {
-      if (isBody) {
-        document.removeEventListener('scroll', scrollHandler)
-      } else {
-        element.removeEventListener('scroll', scrollHandler)
-      }
+    // 若无滚动载体，报错并退出
+    if (typeof element === 'undefined') {
+      console.error('Need Scroll Container!')
+      return
     }
 
-    function onScroll(e) {
-      const scrollTop = isBody
-        ? document.documentElement.scrollTop
-        : this.scrollTop
+    // 判断滚动载体是否为 body
+    const isBody = element === document.body
 
+    // 初始化滚动事件节点状态
+    const distanceEventsStatus = distanceEvents.map(
+      ({ distance }) => (element.scrollTop > distance ? OUTSIDE : INSIDE)
+    )
+
+    /**
+     * 若使用触底函数，则启用相关逻辑
+     */
+    if (isFunction(onEndReached)) {
+      // 触底函数是否被冻结，将此值置为 true 则停止使用触底函数
+      let isEndReacherFreeze = false
+
+      /**
+       * 每次触发 onEndReached 函数后会自动阻止下一次触发，需要执行 done 函数来释放阻止
+       * （例如滚动到底部后开始做网络请求时，再次滚动到底部不会触发二次请求）
+       * 如果 isOver 为 true 则不会再触发后续的 onEndReached
+       * （例如已经加载了全部页，不需要再监听触底事件）
+       */
+      const done = isOver => {
+        if (!isOver) {
+          isEndReacherFreeze = false
+          return
+        }
+
+        if (distanceEvents.length === 1) {
+          this.destroy()
+        }
+      }
+
+      const endReachedEvents = {
+        distance() {
+          return (
+            element.scrollHeight - element.offsetHeight - distanceToReachEnd
+          )
+        },
+        onGoingOut: () => {
+          if (isEndReacherFreeze) return
+          isEndReacherFreeze = true
+          onEndReached(done)
+        }
+      }
+
+      distanceEvents.push(endReachedEvents)
+      distanceEventsStatus.push(INSIDE)
+    }
+
+    // 若无滚动事件可用，报错并退出
+    if (distanceEvents.length === 0) {
+      console.error('Need Distance Events!')
+      return
+    }
+
+    const onScroll = e => {
       e.stopPropagation()
 
+      // body 元素的 scrollTop 取值时不同于普通元素
+      const scrollTop = isBody
+        ? document.documentElement.scrollTop
+        : element.scrollTop
+
+      // 每次 onScroll 触发时对各事件的监听
       distanceEvents.forEach(
         (
           {
             distance = -1,
-            onGoingIn = () => console.log('on going in'),
-            onGoingOut = () => console.log('on going out')
+            onGoingIn = () => undefined,
+            onGoingOut = () => undefined
           },
           idx
         ) => {
+          if (isFunction(distance)) {
+            distance = distance()
+          }
+
+          // 仅当状态值变更时触发 onGoingIn、onGoingOut 函数
           switch (distanceEventsStatus[idx]) {
-            case 'inside':
+            case INSIDE:
               if (scrollTop > distance) {
-                onGoingOut.call(this, self)
-                distanceEventsStatus[idx] = 'outside'
+                onGoingOut()
+                distanceEventsStatus[idx] = OUTSIDE
               }
               break
-            case 'outside':
+            case OUTSIDE:
               if (scrollTop <= distance) {
-                onGoingIn.call(this, self)
-                distanceEventsStatus[idx] = 'inside'
+                onGoingIn()
+                distanceEventsStatus[idx] = INSIDE
               }
               break
           }
         }
       )
-
-      if (
-        self.endReacher.working &&
-        !self.endReacher.pausing &&
-        this.scrollTop + this.offsetHeight + distanceToReachEnd >
-          this.scrollHeight
-      ) {
-        self.endReacher.pause()
-        onEndReached.call(this, function done(isOver) {
-          self.endReacher.continue()
-
-          if (isOver) {
-            if (distanceEvents.length === 0) {
-              self.destroy()
-            } else {
-              self.endReacher.working = false
-            }
-          }
-        })
-      }
     }
 
-    scrollHandler = e => {
+    /**
+     * 使用 requestAnimationFrame 优化 scroll 监听
+     */
+    let rAFLock = false
+    const scrollHandler = e => {
       if (rAFLock) return
       requestAnimationFrame(() => {
         onScroll.call(element, e)
@@ -87,24 +125,19 @@ export default class TinyScrollListener {
       rAFLock = true
     }
 
-    // .addEventListener('scroll', 在 body 上无效
-    if (isBody) {
-      document.addEventListener('scroll', scrollHandler)
-    } else {
-      element.addEventListener('scroll', scrollHandler)
-    }
+    // .addEventListener('scroll', 在 body 上无效，需调用到 document 上
+    ;(isBody ? document : element).addEventListener(
+      SCROLL_EVENT_NAME,
+      scrollHandler
+    )
 
-    return self
-  }
+    this.destroy = () =>
+      // .removeEventListener('scroll', 在 body 上无效，需调用到 document 上
+      (isBody ? document : element).removeEventListener(
+        SCROLL_EVENT_NAME,
+        scrollHandler
+      )
 
-  endReacher = {
-    working: true,
-    pausing: false,
-    pause() {
-      this.pausing = true
-    },
-    continue() {
-      this.pausing = false
-    }
+    return this
   }
 }
